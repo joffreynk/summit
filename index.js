4d5d9f82e1d7426d8b025174aa4808f76a0d1381
Joffrey <joyjoffrey@gmail.com>
2022-05-26 00:56:27 +0200

    finalize design and work and start put js data

---
+++ index.js
@@ -0,0 +1,8 @@
+const data = [
+    {
+        img:'./images/img1.png',
+        name:'john  elart',
+        job:'computer assistant at econet',
+        description:'He did Computer science at Summit Internation Institute and and he selected to be the best learner of the year'
+    }
+]
